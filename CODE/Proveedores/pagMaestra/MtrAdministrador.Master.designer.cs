a2d542212a0e5f4b32c61295130557dc5e23e24b
Rogelio Sánchez Gómez <rogelio.sanchez@sf-solutionfactory.com>
2023-02-10 16:46:16 -0600

    20230210

--- CODE/Proveedores/pagMaestra/MtrAdministrador.Master.designer.cs
+++ CODE/Proveedores/pagMaestra/MtrAdministrador.Master.designer.cs
@@ -7,11 +7,13 @@
 // </generado automáticamente>
 //------------------------------------------------------------------------------
 
-namespace Adminsitrator.Proveedores {
-    
-    
-    public partial class MtrProveedor {
-        
+namespace Proveedores.administrator
+{
+
+
+    public partial class MtrProveedor
+    {
+
         /// <summary>
         /// Control head.
         /// </summary>
@@ -20,7 +22,7 @@ namespace Adminsitrator.Proveedores {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder head;
-        
+
         /// <summary>
         /// Control form1.
         /// </summary>
@@ -29,7 +31,7 @@ namespace Adminsitrator.Proveedores {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
+
         /// <summary>
         /// Control lblUsuario.
         /// </summary>
@@ -38,7 +40,7 @@ namespace Adminsitrator.Proveedores {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label lblUsuario;
-        
+
         /// <summary>
         /// Control btnCerrarSesion.
         /// </summary>
@@ -47,7 +49,7 @@ namespace Adminsitrator.Proveedores {
         /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Button btnCerrarSesion;
-        
+
         /// <summary>
         /// Control ContentPlaceHolder1.
         /// </summary>
